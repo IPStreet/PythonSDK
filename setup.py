47ef5f80dbbb9530561659db3619427a4ed2531a
Reed Jessen <reed@algo.ventures>
2016-12-06 13:22:39 -0800

    Fixed Typos

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from distutils.core import setup
 setup(
   name = 'IPStreet',
   packages = ['IPStreet'], # this must be the same as the name above
-  version = '0.1',
-  description = 'A Pythin SDK for interacting with the IP Street API',
+  version = 'v0.1',
+  description = 'A Python SDK for interacting with the IP Street API',
   author = 'Reed Jessen',
   author_email = 'Reed@IPStreet.com',
   url = 'https://github.com/IPStreet/PythonSDK', # use the URL to the github repo

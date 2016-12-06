dc63fe7c083bfbc2c4a5338fc846501ef3b3a2af
Reed Jessen <reed@algo.ventures>
2016-12-05 17:19:01 -0800

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'IPStreet',
+  packages = ['IPStreet'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A Pythin SDK for interacting with the IP Street API',
+  author = 'Reed Jessen',
+  author_email = 'Reed@IPStreet.com',
+  url = 'https://github.com/IPStreet/PythonSDK', # use the URL to the github repo
+  download_url = 'https://github.com/peterldowns/mypackage/tarball/0.1', # I'll explain this in a second
+  keywords = ['patent', 'SDK', 'IP Street', 'API', 'wrapper'], # arbitrary keywords
+  classifiers = [],
+)

e16d8b93adcaa1696f1e0b70292ff6d96ce43275
Reed Jessen <reed@algo.ventures>
2016-12-21 18:50:25 -0800

    Included required dependencies

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 from distutils.core import setup
+
 setup(
-  name = 'IPStreet',
-  packages = ['IPStreet'], # this must be the same as the name above
-  version = 'v0.1.1',
-  description = 'A Python SDK for interacting with the IP Street API',
-  author = 'Reed Jessen',
-  author_email = 'Reed@IPStreet.com',
-  url = 'https://github.com/IPStreet/PythonSDK', # use the URL to the github repo
-  download_url = 'https://github.com/IPStreet/PythonSDK/archive/v0.1.1.tar.gz', # I'll explain this in a second
-  keywords = ['patent', 'SDK', 'IP Street', 'API', 'wrapper'], # arbitrary keywords
-  classifiers = [],
+    name='IPStreet',
+    packages=['IPStreet'],  # this must be the same as the name above
+    version='v0.1.1',
+    description='A Python SDK for interacting with the IP Street API',
+    author='Reed Jessen',
+    author_email='Reed@IPStreet.com',
+    url='https://github.com/IPStreet/PythonSDK',  # use the URL to the github repo
+    download_url='https://github.com/IPStreet/PythonSDK/archive/v0.1.1.tar.gz',  # I'll explain this in a second
+    install_requires=['requests'],
+    keywords=['patent', 'SDK', 'IP Street', 'API', 'wrapper'],  # arbitrary keywords
+    classifiers=[],
 )
